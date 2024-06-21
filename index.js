8132b695046a8748e697679d442ba750a2595dac
BentlyM <metayerbently@gmail.com>
2024-06-21 17:13:04 -0400

    yes

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
 const onTileClick = (container) => {
     container.addEventListener('click',(event) => {
         if(event.target === container) return;
+
     });
 }
 
-(function(){
+const GameBoard = (function(){
     const container = document.querySelector('.container');
-    const MAX_TILES = 9;
-    for(let i = 0; i < MAX_TILES; i++){
+    const gameBoard = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
+
+    for(let i = 0; i < gameBoard.length; i++){
         const tile = document.createElement('div');
         tile.classList.add('tiles');
         tile.id = `tile${i}`

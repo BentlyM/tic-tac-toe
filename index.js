cc7d8eff8e5094f19a98d62e5bd002e6306f15aa
BentlyM <metayerbently@gmail.com>
2024-06-21 12:58:48 -0400

    added tiles and event listner to tiles

---
+++ index.js
@@ -0,0 +1,21 @@
+const gameBoard = [];
+const player = {};
+const gameFlow = {};
+
+const onTileClick = (container) => {
+    container.addEventListener('click',(event) => {
+       
+    });
+}
+
+(function(){
+    const container = document.querySelector('.container');
+    const MAX_TILES = 9;
+    for(let i = 0; i < MAX_TILES; i++){
+        const tile = document.createElement('div');
+        tile.classList.add('tiles');
+        tile.id = `tile${i}`
+        container.appendChild(tile);
+    }
+    onTileClick(container);
+})();

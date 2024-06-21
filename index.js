3c3f45fa50116166dadb01da78e7902c96b5c9e5
BentlyM <metayerbently@gmail.com>
2024-06-21 19:43:03 -0400

    moved some stuff around

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-
+const availableSquares = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+];
 
 const onTileClick = (container) => {
-    container.addEventListener('click',(event) => {
-        if(event.target === container) return;
-
-    });
+    container.addEventListener('click',(event)=>{
+        if(event.target === container) throw `${event.target} === ${container}`;
+        console.log(
+            `Tile ${event.target.id} was clicked`
+        )
+    })
 }
 
 const GameBoard = (function(){
     const container = document.querySelector('.container');
-    const gameBoard = [
-        "", "", "",
-        "", "", "",
-        "", "", ""
+    let currentPlayer = '';
+    let player2 = ''
+    let computerBot = '';
+    let gameOver = false;
+    let winningCombinations = [
+        ['0','1','2'],
+        ['3','4','5'],
+        ['6','7','8'],
+        ['0','3','6'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['0','4','8'],
+        ['2','4','6']
     ];
 
-    for(let i = 0; i < gameBoard.length; i++){
+    for(let i = 0; i < availableSquares.length; i++){
         const tile = document.createElement('div');
         tile.classList.add('tiles');
-        tile.id = `tile${i}`
+        tile.id = i;
         container.appendChild(tile);
     }
-    onTileClick(container);
 })();
